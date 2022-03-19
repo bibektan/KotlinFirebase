c37a56d5dccde1286c6b788692408de9c399edd0
bibek <bibektan@gmail.com>
2022-03-19 14:02:41 +0545

    Retrofit REST API

--- app/src/main/java/com/manish/projectapp/DashBoardActivity.kt
+++ app/src/main/java/com/manish/projectapp/DashBoardActivity.kt
@@ -8,7 +8,6 @@ import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.firestore.FirebaseFirestore
 import kotlinx.android.synthetic.main.activity_dash_board.*
-import kotlinx.android.synthetic.main.profilefragment.*
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
---
+++ app/src/main/java/com/manish/projectapp/MyData.kt
@@ -0,0 +1,3 @@
+package com.manish.projectapp
+
+class MyData : ArrayList<MyDataItem>()
